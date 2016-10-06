6803b4cc7b21ed0f3ed01a332c3e6b48207407ad
caromine <caromine7@gmail.com>
2016-10-06 11:01:55 -0500

    added files

---
+++ App1/App1/AddNums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class AddNums
+    {
+        public static int addNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
